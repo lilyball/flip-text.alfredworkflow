4b7372b9e60474d17163a3741ba7397e2ec8f6e2
Kevin Ballard <kevin@sb.org>
2015-03-23 23:12:18 -0700

    Provide a few default flips when no argument is given

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ use std::io::prelude::*;
 mod flip;
 
 fn main() {
-    let arg = env::args().skip(1).next().expect("expected argument");
-    match process_argument(arg) {
+    let arg = env::args().skip(1).next();
+    match process_argument(arg.as_ref().map_or("", |s| &s[..])) {
         Err(err) => {
             let _ = writeln!(&mut io::stderr(), "I/O error: {}", err);
             env::set_exit_status(1);
@@ -23,9 +23,13 @@ fn main() {
     }
 }
 
-fn process_argument(arg: String) -> io::Result<()> {
+fn process_argument(arg: &str) -> io::Result<()> {
+    let arg = strip(arg);
+    if arg.is_empty() {
+        return default_output();
+    }
     let mut xmlw = try!(alfred::XMLWriter::new(io::stdout()));
-    for op in flip::process_text(strip(&arg)) {
+    for op in flip::process_text(arg) {
         let title = format!("{}", op);
         let mut builder = alfred::ItemBuilder::new(&title[..]).arg(&title[..]);
         match op {
@@ -38,14 +42,26 @@ fn process_argument(arg: String) -> io::Result<()> {
     try!(xmlw.close()).flush()
 }
 
+fn default_output() -> io::Result<()> {
+    let mut xmlw = try!(alfred::XMLWriter::new(io::stdout()));
+    try!(xmlw.write_item(&alfred::ItemBuilder::new("Flip \u{2026}").valid(false).arg("")
+                                              .subtitle("Flip typed text").into_item()));
+    for &s in ["(╯°□°）╯︵ ┻━┻", "┬─┬ノ( º _ ºノ)", "┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻ "].iter() {
+        try!(xmlw.write_item(&alfred::ItemBuilder::new(s).valid(true).arg(s)
+                                                  .uid(format!("flip {}", s)).into_item()));
+    }
+    try!(xmlw.close()).flush()
+}
+
 fn strip(mut s: &str) -> &str {
     let prefixes: &[&str] = &["(╯°□°）╯︵", "(╯ಠ_ಠ）╯︵", "(ﾉ `Д´)ﾉ ﾐ"];
     let suffixes: &[&str] = &["ノ( º _ ºノ)"];
+    s = s.trim();
     for prefix in prefixes {
-        s = s.trim_left_matches(prefix);
+        s = s.trim_left_matches(prefix).trim_left();
     }
     for suffix in suffixes {
-        s = s.trim_right_matches(suffix);
+        s = s.trim_right_matches(suffix).trim_right();
     }
     s
 }
